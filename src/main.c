f7a4a270904b301fd6eaeeb04506d315614625e6
Rebecca Lang <rebeccalang@rebeccas-air.lan>
2023-02-21 18:48:56 +1300

    initial commit

---
+++ src/main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+
+int main () {
+	printf ("listtest\n");
+	item_t *root;
+	return 0;
+} 
+
+item_t *lt_add (item_t *root, uint8_t val) {
+	
+	}
+
+	return NULL;
+}
